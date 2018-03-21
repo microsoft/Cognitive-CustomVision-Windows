38cf67100a9ff4a8440920d2450452e5d2aee0f0
Diego <dicolomb@microsoft.com>
2018-03-21 15:07:48 +0000

    remove old project and unused files

--- ClientLibrary - Copy/Generate-ClientLibraries.ps1
+++
@@ -1,7 +0,0 @@
-
-# Prediction
-Write-Host "Generating Predicton API files"
-AutoRest --version 1.0.1-20170508-2300-nightly -Namespace "Microsoft.Cognitive.CustomVision" -Input "Microsoft.Cognitive.CustomVision.Prediction\Prediction-swagger.json" -AddCredentials -SyncMethods All -OutputDirectory "Microsoft.Cognitive.CustomVision.Prediction\Generated" -CodeGenerator "CSharp"
-
-Write-Host "Generating Training API files"
-AutoRest --version 1.0.1-20170508-2300-nightly -Namespace "Microsoft.Cognitive.CustomVision" -Input "Microsoft.Cognitive.CustomVision.Training\Training-swagger.json" -AddCredentials -SyncMethods All -OutputDirectory "Microsoft.Cognitive.CustomVision.Training\Generated" -CodeGenerator "CSharp"
